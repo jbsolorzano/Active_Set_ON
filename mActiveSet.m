7d64821dc4942b7af1dcef112f9e05b855fd72bd
jbsolorzano <jb_solorzano@hotmail.com>
2021-10-12 14:44:41 -0500

    Primera versión

--- mActiveSet.m
+++ mActiveSet.m
@@ -13,43 +13,68 @@ function [xmin, iter] = mActiveSet(G, c, A, b, D, d)
 
 
 % Definir el working set  W c CA
-W = [];
 sigue = 0;
 iter = 0;
 [m, ~]  = size(A); % restricciones de igualdad
+W = [1:m];
 R = length(d); % restricciones de desigualdad
 % restricciones activas de desigualdad en el working set
 
-xk  = punto_x0(A,D,b,d);
-
+xk = linprog(zeros(length(A),1),D,d,A,b);
+%xk = [3 0 0]';
 %Resolvemos subproblema cuadrático
-%min (1/2)p'Gp + p'g sa Ak*p = phi
-
-while (sigue == 0)
-    CA = find(D*xk==d);
-    Ak = [A; D(W,:)]; % matriz de restricciones activas
-    
-    g  = Q*xk + c;
+%min (1/2)p'Gp + p'g sa Ak*p 
+CA = find(D*xk==d);
+while ((sigue == 0) &&(iter < 10))
+    xk
+    W = sort(W);
+    Ak = [A; D(CA,:)]; % matriz de restricciones activas
+    Ak
+    r  = length(CA);
     b1 = zeros(m+r,1);
     
+    g  = G*xk + c;
     if (isempty(Ak))
         % Problema sin restricciones
         p = -G\g;
     else
         p  = esp_nulo( G, Ak, g, b1);
     end
-    
+    p
     %RAMA 2
-     if (norm(p)<1.e-12) %le agregamos una tolerancia
-        % calculamos los multiplicadores de Lagrange
-        mu;
-        
-        if (mu >= 0)
+    if (norm(p)<1.e-12) %le agregamos una tolerancia
+        % calculamos los multiplicadores de Lagrange despejando de las CNPO
+        lambda = -(Ak*Ak')\(Ak*g);
+        lambda
+        % encontrar A'
+        if (lambda(m+1:end) >= 0)
             xmin = xk;
-            sigue = 1; 
+            sigue = 1;
+        else
+            %Todas las mu [ lambdas de desigualdad (m+1:end) menores a cero 
+            %y queremos quitar solo una,la mas negativa]
+            j = find(lambda == min(lambda(m+1:end)), 1, 'last');
+            CA(j-m) = []; % quitamos la restriccion j
+            %W(end+1)=j+m;
         end
-     end
-iter = iter +1;  
 
+    else  % p ~= 0
+        jna = find(~ismember(1:R,CA)); % restricciones no activas
+        D(jna,:)*p;
+        %jna;
+
+        jna1 = jna(D(jna,:)*p > 0);
+        [tj,ind] = min((d(jna1)-D(jna1,:)*xk) ./ (D(jna1,:)*p))
+
+        if (tj < 1)
+            xk = xk + tj * p;
+            CA = [CA jna1(ind)]; % agregamos la restriccion que se volvió activa
+        else
+            xk = xk + p;
+        end              
+
+    end
+    
+    iter = iter +1;  
 end
 end
