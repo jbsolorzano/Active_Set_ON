e4af9f6357c8c459725c0d369dcb1b85169429d4
jbsolorzano <jb_solorzano@hotmail.com>
2021-10-19 19:40:27 -0500

    Completo

--- mActiveSet.m
+++ mActiveSet.m
@@ -1,4 +1,4 @@
-function [xmin, iter] = mActiveSet(G, c, A, b, D, d)
+function [xmin, iter,valor_min] = mActiveSet(G, c, A, b, D, d, x0, W0, maxIter)
 % Queremos minimizar el problema
 % 1/2 * x'*G*x + x'*c
 % s.a. A*x = b
@@ -10,30 +10,60 @@ function [xmin, iter] = mActiveSet(G, c, A, b, D, d)
 % b.- vector de mx1
 % D.- matriz de rxn 
 % d.- vector de rx1
+% x0.- vector de nx1, solución particular
+% W0.- Conjunto de índices iniciales de desigualdades donde se da igualdad;
+
+%NOTA_1: Índices ={1,2,...,m,m+1,m+2,...,m+r}. Los primeros m corresponden a
+    % las restricciones de igualdad; los últimos r índices corresponden a
+    % las desigualdades.
+%NOTA_2: Si el punto x0 se manda vacío, se encuentra una solución
+%particular con ligprog.
+%NOTA_3: Si el conjunto W0 se manda vacío, se toma por default W0= A(x0).
 
 
-% Definir el working set  W c CA
-sigue = 0;
+
+if (isempty(maxIter))
+    maxIter = 100;
+end
+bandera = 0;
 iter = 0;
 [m, ~]  = size(A); % restricciones de igualdad
-W = [1:m];
+if (isempty(A))
+    E = [];
+    m = 0;
+else
+    [m, ~]  = size(A); % restricciones de igualdad
+    E = [1:m]';
+end
 R = length(d); % restricciones de desigualdad
-% restricciones activas de desigualdad en el working set
 
-xk = linprog(zeros(length(A),1),D,d,A,b);
-%xk = [3 0 0]';
+xk = x0;
+
+%Encontramos una solución x0 en caso que no se proporcione
+if(isempty(x0))
+    num_var = max(size(A,2),size(D,2));
+    xk = linprog(zeros(num_var,1),D,d,A,b);
+end
+
 %Resolvemos subproblema cuadrático
 %min (1/2)p'Gp + p'g sa Ak*p 
-CA = find(D*xk==d);
-while ((sigue == 0) &&(iter < 100))
-    xk
-    CA
+
+if(isempty(W0))
+    CA = find(D*xk==d); %Conjunto activo de desigualdades; W0= A(x0)
+end
+
+CA = CA-m;
+CA = sort(CA);
+
+while ((bandera == 0) &&(iter < maxIter))
+    W= [E; (CA+m)]; % Conjunto activo
     W = sort(W);
-    Ak = [A; D(CA,:)]; % matriz de restricciones activas
-    size(Ak)
-    r  = length(CA);
-    b1 = zeros(m+r,1);
     
+    %Resolvemos subproblema cuadrático
+    %min (1/2)p'Gp + p'g sa Ak*p 
+    Ak = [A; D(CA,:)]; % matriz de restricciones activas
+    r  = length(CA);
+    b1 = zeros(m+r,1);   
     g  = G*xk + c;
     if (isempty(Ak))
         % Problema sin restricciones
@@ -41,41 +71,40 @@ while ((sigue == 0) &&(iter < 100))
     else
         p  = esp_nulo( G, Ak, g, b1);
     end
-    p
+
     %RAMA 2
     if (max(abs(p))<10^(-9)) %le agregamos una tolerancia
         % calculamos los multiplicadores de Lagrange despejando de las CNPO
         lambda = -(Ak*Ak')\(Ak*g);
-        lambda
         % encontrar A'
         if (lambda(m+1:end) >= 0)
             xmin = xk;
-            sigue = 1;
+            bandera = 1;
         else
             %Todas las mu [ lambdas de desigualdad (m+1:end) menores a cero 
             %y queremos quitar solo una,la mas negativa]
             j = find(lambda == min(lambda(m+1:end)), 1, 'last');
             CA(j-m) = []; % quitamos la restriccion j
-            %W(end+1)=j+m;
         end
-
-    else  % p ~= 0
+        
+    %RAMA 1
+    else  % p != 0
         jna = find(~ismember(1:R,CA)); % restricciones no activas
-        %D(jna,:)*p;
-        %jna;
-
+        
         jna1 = jna(D(jna,:)*p > 0);
-        [alpha,ind] = min((d(jna1)-D(jna1,:)*xk) ./ (D(jna1,:)*p))
+        [alpha,ind] = min((d(jna1)-D(jna1,:)*xk) ./ (D(jna1,:)*p));
 
         if (alpha < 1)
             xk = xk + alpha * p;
-            jna1(ind)
-            CA = [CA' jna1(ind)]'; % agregamos la restriccion que se volvió activa
+            CA = [CA; jna1(ind)]; % agregamos la restriccion que se volvió activa
         else
             xk = xk + p;
-        end              
+        end
 
     end   
-    iter = iter +1;  
+    iter = iter + 1; 
+    fprintf("\nConjunto activo en la %d-ésima iteración:",iter)
+    W'
 end
+valor_min = (xmin'*G*xmin)/2+xmin'*c;
 end
